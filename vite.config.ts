d7da467edf58083a743a5994f76d8c0ab509776d
3dd3v <cr7pt0curr3ncy@gmail.com>
2024-07-25 22:45:46 +0300

    deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,9 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", // This tells Vite to use relative paths
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`,
-      },
-    },
-  },
+  base: "/buddha/", // YOUR REPO NAME HERE
 });
