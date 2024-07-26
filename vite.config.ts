2c51bc6f40206bcf0244b37a7a2cc595f41926b6
3dd3v <cr7pt0curr3ncy@gmail.com>
2024-07-26 20:56:38 +0300

    fixed base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/buddha/", // YOUR REPO NAME HERE
+  base: "/", // YOUR REPO NAME HERE
 });
 
 
